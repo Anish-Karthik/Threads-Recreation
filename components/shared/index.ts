47a484500c614989381d9caf1b720e8bf234821a
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-08-05 19:14:28 +0530

    completed topbar

---
+++ components/shared/index.ts
@@ -0,0 +1,4 @@
+export { default as Bottombar } from './Bottombar';
+export { default as Topbar } from './Topbar';
+export { default as LeftSidebar } from './LeftSidebar';
+export { default as RightSidebar } from './RightSidebar';
