136354e29d209a30eb031fb90186406acd36864d
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-08-05 18:06:07 +0530

    completed auth using clerk

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main>
+      <h1 className='head-text'>Onboarding</h1>
+    </main>
+  )
+}
+
+export default page
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,9 @@
-import './globals.css'
+import '../globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+
+import '../globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +18,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ ClerkProvider>
   )
 }
