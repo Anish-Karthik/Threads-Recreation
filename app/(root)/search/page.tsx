efd7a3b09fc0873e3f8da079fa772d24113d3fa1
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-08-26 20:05:21 +0530

    Fully functional search

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,3 +1,4 @@
+import UserCard from '@/components/cards/UserCard';
 import ProfileHeader from '@/components/shared/ProfileHeader';
 import ThreadsTab from '@/components/shared/ThreadsTab';
 import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
@@ -28,7 +29,24 @@ const SearchPage = async () => {
     <section>
       <h1 className="head-text mb-10">Search</h1>
       {/* Search Bar */}
-
+      <div className='mt-14 flex flex-col gap-9'>
+        {result.users.length === 0 ? (
+          <p className='no-result'>No users</p>
+        ) : (
+          <>
+            {result.users.map((person) => (
+              <UserCard 
+                key={person.id} 
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image} 
+                personType='User'            
+              />
+            ))}
+          </>
+        )}
+      </div>
 
     </section>
   )
