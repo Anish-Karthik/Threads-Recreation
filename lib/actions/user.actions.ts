90df056d5f00ce2b4702d0c011226317d73a69f4
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-08-26 10:15:00 +0530

    Created and connected mongoDb schema

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,1 +1,37 @@
 "use server"
+
+import { revalidatePath } from "next/cache";
+import User from "../models/user.model"
+import { connectToDB } from "../mongoose"
+
+
+export async function updateUser(
+  userId: string,
+  username: string,
+  name: string,
+  image: string,
+  bio: string,
+  path: string,
+): Promise<void> {
+  connectToDB()
+  try {
+    await User.findOneAndUpdate(
+      { id: userId },
+      {
+        username: username.toLowerCase(),
+        name,
+        image,
+        bio,
+        onboarded: true,
+        updatedAt: new Date(),
+      },
+      { upsert: true }
+    );
+    if (path === '/profile/edit') {
+      revalidatePath(path)
+    }
+  } catch (error: any) {
+    console.log(error)
+    throw new Error(`Failed to create/update user: ${error.message}`)
+  }
+}
