19c6cff470b8cb032a94e67aff09cc1bfdde6ea4
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-10-07 16:35:08 +0530

    Bug

--- components/thread-actions/LikeThread.tsx
+++ components/thread-actions/LikeThread.tsx
@@ -18,7 +18,7 @@ interface LikeThreadProps {
 const LikeThread = ({isLiked, threadId, userId, likeCount = 0 , path}: LikeThreadProps) => {
 
   const [liked, setLiked] = useState(isLiked);
-  // const [likes, setLikes] = useState(likeCount);
+  const [likes, setLikes] = useState(likeCount);
   const user = useAuth();
   const router = useRouter();
   const toggleHeart = async () => {
@@ -29,8 +29,8 @@ const LikeThread = ({isLiked, threadId, userId, likeCount = 0 , path}: LikeThrea
         toast.error('Something went wrong');
         return;
       }
+      setLikes(result.likes);
       setLiked(result.isLiked);
-      // setLikes(result.likes);
     } catch (error) {
       toast.error('Something went wrong');
     }
@@ -40,7 +40,7 @@ const LikeThread = ({isLiked, threadId, userId, likeCount = 0 , path}: LikeThrea
   return (
     <div className='flex items-center justify-start gap-1 text-light-4'>
       <Image src={liked?'/assets/heart-filled.svg': '/assets/heart-gray.svg'} alt='heart' width={24} height={24} className='cursor-pointer object-contain' onClick={toggleHeart}/>
-      {likeCount + (liked? 1 : 0) > 0  && likeCount + (liked? 1 : 0) === likeCount ? likeCount + 1: likeCount + (liked? 1 : 0)}
+      {likes > 0 && likes}
     </div>
   )
 }
